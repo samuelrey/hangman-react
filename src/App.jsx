e05225881bd07f704aa60b4d6562b8be5744622e
Samuel Rey <samuelrey010@gmail.com>
2021-06-11 16:25:03 -0700

    Conditionally render Game

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
-import React from "react";
-import "./App.css";
+import React, { useState } from "react";
+import Game from "./Game";
 
 function App() {
-    return <div className="App"></div>;
+    const [id, setId] = useState(null);
+
+    const newGame = async () => {
+        // TODO fetch response from API
+        setId("1447");
+    };
+
+    if (id) {
+        return (
+            <div className="App">
+                <h2>hangman</h2>
+                <main>
+                    <Game />
+                </main>
+            </div>
+        );
+    }
+    return (
+        <div className="App">
+            <h2>hangman</h2>
+            <button className="btn" onClick={() => newGame()}>
+                New
+            </button>
+        </div>
+    );
 }
 
 export default App;
