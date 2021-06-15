86251a8cf0ee7471f02eb851bfd1404224a38bf9
Samuel Rey <samuelrey010@gmail.com>
2021-06-14 21:52:22 -0700

    Stub logic to handle guess

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,18 @@ function App() {
     const [game, setGame] = useState(null);
 
     const newGame = async () => {
-        try {
-            const response = await fetch("http://localhost:14420/new");
-            const json = await response.json();
-            setGame(json);
-        } catch (error) {
-            console.log(error);
-        }
+        // try {
+        //     const response = await fetch("http://localhost:14420/new");
+        //     const json = await response.json();
+        //     setGame(json);
+        // } catch (error) {
+        //     console.log(error);
+        // }
+        setGame(fakeGame());
+    };
+
+    const handleGuess = (letter) => {
+        console.log("Guessed: ", letter);
     };
 
     if (game !== null) {
@@ -19,7 +24,7 @@ function App() {
             <div className="App">
                 <h2>hangman</h2>
                 <main>
-                    <Game game={game} />
+                    <Game game={game} handleGuess={handleGuess} />
                 </main>
             </div>
         );
@@ -34,5 +39,13 @@ function App() {
         </div>
     );
 }
+
+const fakeGame = () => {
+    return {
+        id: "14420",
+        current: "____",
+        answer: null,
+    };
+};
 
 export default App;
--- src/Game.jsx
+++ src/Game.jsx
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Game = ({ game }) => {
-    console.log(game);
+const Game = ({ game, handleGuess }) => {
+    const [letter, setLetter] = useState("");
+
+    const handleChange = ({ target: { value } }) => {
+        setLetter(value);
+    };
+
     return (
         <div>
             <h2 className="current">{game.current}</h2>
             <p>
-                <input className="letter-input"></input>
-                <button className="btn">Guess</button>
+                <input
+                    className="letter-input"
+                    maxLength={1}
+                    onChange={handleChange}
+                />
+
+                <button
+                    className="btn"
+                    onClick={() => {
+                        handleGuess(letter);
+                    }}
+                >
+                    Guess
+                </button>
             </p>
         </div>
     );
